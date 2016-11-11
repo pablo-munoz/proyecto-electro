09ea2f1e62d36fa72213bc5d14074fca23468f1c
Pablo Muñoz Haro <home@Pablos-MacBook-Air.local>
2016-11-11 12:03:39 -0600

    Adds background. Improves UI. Refactors code.

--- projectv2.js
+++ projectv2.js
@@ -11,6 +11,9 @@ const PROTON_MASS     = 1.6727 * Math.pow(10, -27);
 const NEUTRON_MASS    = 1.6750 * Math.pow(10, -27);
 const ELECTRON_MASS   =  9.110 * Math.pow(10, -31);
 
+const PIXELS_PER_METER = 1;
+const VECTOR_WIDTH = 2;
+
 class Particle {
     // x, y, radius
     constructor(args) {
@@ -27,35 +30,39 @@ class Particle {
         }));
         this.forceX = 0;
         this.forceY = 0;
-        if (this.charge > 0) {
-            this.color = 'red';
-        } else if (this.charge < 0) {
-            this.color = 'blue';
-        }
-
     }
 
     draw() {
+        this.forceVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
+        this.forceVector.strokeWidth = VECTOR_WIDTH;
+        this.forceVector.strokeColor = 'rgba(255, 255, 255, 0.5)';
+        this.accelVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
+        this.accelVector.strokeWidth = VECTOR_WIDTH;
+        this.accelVector.strokeColor = 'rgba(255, 0, 0, 0.5)';
+        this.velocityVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
+        this.velocityVector.strokeWidth = VECTOR_WIDTH;
+        this.velocityVector.strokeColor = 'rgba(0, 255, 0, 0.5)';
         this.circle = new Path.Circle(new Point(this.x, this.y), this.radius);
-        this.circle.fillColor = this.color;
         this.circle.onMouseDrag = _.bind(function(event) {
             this.circle.translate(event.delta);
+            this.drawAllVectors();
         }, this);
-        this.forceVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
-        this.forceVector.strokeColor = 'black';
-        this.accelVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
-        this.accelVector.strokeColor = 'red';
-        this.velVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
-        this.velVector.strokeColor = 'green';
+        this.setParticleColor();
     }
 
-    on(event, func) {
-        this.circle['on' + _.capitalize(event)] = func;
+    drawVector(whichVector) {
+        this[whichVector + 'Vector'].segments = [this.getPosition(), this.getOffsetByMeters(new Point(this[whichVector + 'X'], this[whichVector + 'Y']))];
+    }
+
+    drawAllVectors() {
+        _.forEach(['force', 'velocity', 'accel'], _.bind(function(whichVector) {
+            this.drawVector(whichVector);
+        }, this));
     }
 
     reactToElectricFieldDueTo(otherParticle) {
-        const distanceX = (this.circle.position.x - otherParticle.circle.position.x);
-        const distanceY = (this.circle.position.y - otherParticle.circle.position.y);
+        const distanceX = (this.circle.position.x - otherParticle.circle.position.x) / PIXELS_PER_METER;
+        const distanceY = (this.circle.position.y - otherParticle.circle.position.y) / PIXELS_PER_METER;
         if(distanceX == 0 && distanceY == 0) {
             return;
         }
@@ -71,10 +78,28 @@ class Particle {
         const seconds = milliseconds / 1000;
         this.velocityX += this.accelX * seconds;
         this.velocityY += this.accelY * seconds;
-        this.circle.translate(new Point(this.velocityX, this.velocityY));
-        this.forceVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.forceX), (this.circle.position.y + this.forceY))];
-        this.accelVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.accelX), (this.circle.position.y + this.accelY))];
-        this.velVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.velocityX), (this.circle.position.y + this.velocityY))];
+        this.circle.translate(new Point(this.velocityX * PIXELS_PER_METER, this.velocityY * PIXELS_PER_METER));
+        this.drawAllVectors();
+    }
+
+    setParticleColor() {
+        if (this.charge > 0) {
+            this.circle.fillColor = 'red';
+        } else if (this.charge < 0) {
+            this.circle.fillColor = 'blue';
+        }
+    }
+
+    getPosition() {
+        return new Point(this.circle.position.x, this.circle.position.y);
+    }
+
+    getOffset(offsetPoint) {
+        return this.getPosition().add(offsetPoint);
+    }
+
+    getOffsetByMeters(offsetPointMeters) {
+        return this.getOffset(offsetPointMeters.multiply(PIXELS_PER_METER));
     }
 
 }
@@ -92,7 +117,6 @@ class TwoPointChargeSystem {
 
         this.p0 = new Particle({
             x: WINDOW_WIDTH / 2,
-            //y: WINDOW_HEIGHT / 2 - 30,
             velocityX: 0,
             velocityY: -1,
             charge: ELECTRON_CHARGE*5,
@@ -103,7 +127,6 @@ class TwoPointChargeSystem {
 
         this.p1 = new Particle({
             x: WINDOW_WIDTH / 2 + 30,
-            //y: WINDOW_HEIGHT / 2 + 30,
             velocityX: 0,
             velocityY: 0,
             charge: PROTON_CHARGE*5,
@@ -122,7 +145,13 @@ class TwoPointChargeSystem {
         }, this), this.frameMillis);
         this.disableInputs();
         this.running = true;
-        this.renameStartResetButton();
+        this.renameStartStopButton();
+    }
+
+    stop() {
+        clearInterval(this.refreshIntervalId);
+        this.running = false;
+        this.renameStartStopButton();
     }
 
     reset() {
@@ -131,14 +160,14 @@ class TwoPointChargeSystem {
         this.initialize();
         this.running = false;
         this.enableInputs();
-        this.renameStartResetButton();
+        this.renameStartStopButton();
     }
 
     toggleStatus() {
         if (!this.running) {
             this.start();
         } else {
-            this.reset();
+            this.stop();
         }
     }
 
@@ -150,11 +179,11 @@ class TwoPointChargeSystem {
         $('input').attr('disabled', null);
     }
 
-    renameStartResetButton() {
+    renameStartStopButton() {
         if (this.running) {
-            $('#start-reset-btn').text('Reset');
+            $('#start-stop-btn').text('Stop');
         } else {
-            $('#start-reset-btn').text('Start');
+            $('#start-stop-btn').text('Start');
         }
     }
 
@@ -181,46 +210,63 @@ window.onload = function() {
         components: {
             "particle-controls": {
                 props: ['index', 'particle'],
+                data: function() {
+                    return {
+                        showing: true
+                    };
+                },
                 template: `
-<fieldset style="margin-top: 25">
-    <legend>Particle {{ index }}</legend>
-    <div class="form-group">
-        <div class="input-group">
-            <span class="input-group-addon">q</span>
-            <input class="form-control" type="number" v-model="particle.charge"/>
+<div class="panel panel-warning">
+    <div class="panel-heading unselectable" v-on:click="showing = !showing">
+        <span>Particle {{ index }}</span>
+        <span class="glyphicon glyphicon-chevron-down pull-right" v-show="!showing"></span>
+        <span class="glyphicon glyphicon-chevron-up pull-right" v-show="showing"></span>
+    </div>
+    <div class="panel-body" v-show="showing">
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">q =</span>
+                <input class="form-control" type="number" v-model="particle.charge" v-on:change="particle.setParticleColor()"/>
+                <span class="input-group-addon">C</span>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">m =</span>
+                <input class="form-control" type="number" v-model="particle.mass"/>
+                <span class="input-group-addon">kg</span>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">vx =</span>
+                <input class="form-control" type="number" v-model="particle.velocityX"/>
+                <span class="input-group-addon">m/s</span>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">ax =</span>
+                <input class="form-control" type="number" v-model="particle.accelX"/>
+                <span class="input-group-addon">m/s^2</span>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">vy =</span>
+                <input class="form-control" type="number" v-model="particle.velocityY"/>
+                <span class="input-group-addon">m/s</span>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">ay =</span>
+                <input class="form-control" type="number" v-model="particle.accelY"/>
+                <span class="input-group-addon">m/s^2</span>
+            </div>
         </div>
     </div>
-    <div class="form-group">
-        <div class="input-group">
-            <span class="input-group-addon">m</span>
-            <input class="form-control" type="number" v-model="particle.mass"/>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="input-group">
-            <span class="input-group-addon">vx</span>
-            <input class="form-control" type="number" v-model="particle.velocityX"/>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="input-group">
-            <span class="input-group-addon">ax</span>
-            <input class="form-control" type="number" v-model="particle.accelX"/>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="input-group">
-            <span class="input-group-addon">vy</span>
-            <input class="form-control" type="number" v-model="particle.velocityY"/>
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="input-group">
-            <span class="input-group-addon">ay</span>
-            <input class="form-control" type="number" v-model="particle.accelY"/>
-        </div>
-    </div>
-</fieldset>
+</div>
 `
             }
         }
