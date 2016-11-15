48efb16a0317545270113618e91c2c6258fcaba7
Andres Barro <A00226225@itesm.mx>
2016-11-15 13:19:26 -0600

    Coordenadas en 0, 0

--- projectv2.js
+++ projectv2.js
@@ -9,13 +9,14 @@ const PROTON_CHARGE   = -ELECTRON_CHARGE;
 const PROTON_MASS     = 1.6727 * Math.pow(10, -27);
 const NEUTRON_MASS    = 1.6750 * Math.pow(10, -27);
 const ELECTRON_MASS   =  9.110 * Math.pow(10, -31);
+const SCALE = 0.01;
 
 class Particle {
     // x, y, radius
     constructor(args) {
         _.assign(this, _.defaults(args, {
-            x: WINDOW_WIDTH / 2,
-            y: WINDOW_HEIGHT / 2,
+            x: 0,
+            y: 0,
             radius: 8,
             velocityX: 0,            // m/s
             velocityY: 0,            // m/s
@@ -26,6 +27,8 @@ class Particle {
         }));
         this.forceX = 0;
         this.forceY = 0;
+        this.pForce = 0;
+        this.eField = 0;
         if (this.charge > 0) {
             this.color = 'red';
         } else if (this.charge < 0) {
@@ -35,10 +38,14 @@ class Particle {
     }
 
     draw() {
-        this.circle = new Path.Circle(new Point(this.x, this.y), this.radius);
+        this.circle = new Path.Circle(new Point(this.x / SCALE + WINDOW_WIDTH/2, -this.y / SCALE + WINDOW_HEIGHT/2), this.radius);
         this.circle.fillColor = this.color;
         this.circle.onMouseDrag = _.bind(function(event) {
+            console.log(this.x + ", " + this.y);
             this.circle.translate(event.delta);
+            this.x = (this.circle.position.x - WINDOW_WIDTH/2) * SCALE;
+            this.y = -(this.circle.position.y - WINDOW_HEIGHT/2) * SCALE;
+            console.log(this.x + ", " + this.y);
         }, this);
         this.forceVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
         this.forceVector.strokeColor = 'black';
@@ -46,26 +53,28 @@ class Particle {
         this.accelVector.strokeColor = 'red';
         this.velVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
         this.velVector.strokeColor = 'green';
+        this.fieldVector = new Path.Line(new Point(this.x, this.y), new Point(this.x, this.y));
+        this.fieldVector.strokeColor = 'blue';
     }
 
     on(event, func) {
         this.circle['on' + _.capitalize(event)] = func;
     }
-
+/*
     reactToElectricFieldDueTo(otherParticle) {
-        const distanceX = (this.circle.position.x - otherParticle.circle.position.x);
-        const distanceY = (this.circle.position.y - otherParticle.circle.position.y);
+        const distanceX = (this.x - otherParticle.x);
+        const distanceY = (this.y - otherParticle.y);
         if(distanceX == 0 && distanceY == 0) {
             return;
         }
         const qq = (this.charge * otherParticle.charge);
-        const auxiliarForce = PERMITIVITY * ( ( qq ) / Math.pow(( distanceX * distanceX + distanceY * distanceY), 3/2) );
+        const auxiliarForce = PERMITIVITY * ( ( qq ) / Math.pow(( distanceX * distanceX + distanceY * distanceY), 5/2) );
         this.forceX = distanceX * auxiliarForce;
         this.forceY = distanceY * auxiliarForce;
         this.accelX = this.forceX / this.mass;
         this.accelY = this.forceY / this.mass;
     }
-
+*/
     reactToElectricFieldDueTo2(otherParticles) {
         var distanceX;
         var distanceY;
@@ -74,29 +83,37 @@ class Particle {
         this.forceX = 0;
         this.forceY = 0;
         for(var i = 0; i < otherParticles.length; ++i){
-            distanceX = (this.circle.position.x - otherParticles[i].circle.position.x);
-            distanceY = (this.circle.position.y - otherParticles[i].circle.position.y);
+            distanceX = (this.x - otherParticles[i].x);
+            distanceY = (this.y - otherParticles[i].y);
             if((Math.abs(distanceX) <= 0 && Math.abs(distanceY) <= 0) || this === otherParticles[i]){
-                //console.log("hi");
                 continue;
             }
             qq = (this.charge * otherParticles[i].charge);
-            auxiliarForce = PERMITIVITY * ( ( qq ) / Math.pow(( distanceX * distanceX + distanceY * distanceY), 3/2) );
+            auxiliarForce = PERMITIVITY * ( ( qq ) / Math.pow(( distanceX * distanceX + distanceY * distanceY ), 5/2) );
             this.forceX += distanceX * auxiliarForce;
-            this.forceY += distanceY * auxiliarForce;
+            this.forceY += -distanceY * auxiliarForce;
         }
         this.accelX = this.forceX / this.mass;
-        this.accelY = this.forceY / this.mass;
+        this.accelY = this.forceY / this.mass; 
     }
 
     advanceTime(milliseconds) {
         const seconds = milliseconds / 1000;
         this.velocityX += this.accelX * seconds;
         this.velocityY += this.accelY * seconds;
-        this.circle.translate(new Point(this.velocityX, this.velocityY));
-        this.forceVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.forceX), (this.circle.position.y + this.forceY))];
-        this.accelVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.accelX), (this.circle.position.y + this.accelY))];
-        this.velVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.velocityX), (this.circle.position.y + this.velocityY))];
+        this.x += this.velocityX * seconds;
+        this.y += this.velocityY * seconds;
+        this.circle.translate(new Point((this.velocityX * seconds) / SCALE, (this.velocityY * seconds) / SCALE));
+        this.forceVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.forceX / SCALE), (this.circle.position.y + this.forceY / SCALE))];
+        this.accelVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.accelX / SCALE), (this.circle.position.y + this.accelY / SCALE))];
+        this.velVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + this.velocityX / SCALE), (this.circle.position.y + this.velocityY / SCALE))];
+        this.fieldVector.segments = [new Point(this.circle.position.x, this.circle.position.y), new Point((this.circle.position.x + (this.forceX / this.charge) / SCALE), (this.circle.position.y + (this.forceY / this.charge) / SCALE))];
+        if(this.circle.position.x < 0 || this.circle.position.x > WINDOW_HEIGHT){
+            this.velocityX *= -1;
+        }
+        if(this.circle.position.y < 0 || this.circle.position.y > WINDOW_HEIGHT){
+            this.velocityY *= -1;
+        }
     }
 
 }
@@ -109,54 +126,54 @@ class TwoPointChargeSystem {
 
     initialize() {
         paper.project.activeLayer.removeChildren();
-        this.frameMillis = 1000/60;
+        this.frameMillis = 1000/1000;
         var p0 = new Particle({
-            x: WINDOW_WIDTH / 2 + 30,
-            //y: WINDOW_HEIGHT / 2 - 30,
+            x: 1,
+            y: 1,
             velocityX: 0,
-            velocityY: -1,
-            charge: ELECTRON_CHARGE * 20    ,
+            //velocityY: 16.1,
+            charge: ELECTRON_CHARGE,
             mass: ELECTRON_MASS
         });
 
         var p1 = new Particle({
-            x: WINDOW_WIDTH / 2,
-            //y: WINDOW_HEIGHT / 2 + 30,
+            x: 0,
+            y: 0,
             velocityX: 0,
             velocityY: 0,
-            charge: PROTON_CHARGE * 20  ,
-            mass: PROTON_MASS,
+            charge: PROTON_CHARGE,
+            //mass: PROTON_MASS,
         });
-
+/*
         var p2 = new Particle({
-            x: WINDOW_WIDTH / 2 - 30,
-            //y: WINDOW_HEIGHT / 2 + 30,
+            x: -1,
+            y: 0,
             velocityX: 0,
-            velocityY: 1,
-            charge: ELECTRON_CHARGE * 20    ,
+            velocityY: -15.99999,
+            charge: ELECTRON_CHARGE,
             mass: ELECTRON_MASS,
         });
-
+/*
         var p3 = new Particle({
-            //x: WINDOW_WIDTH / 2 - 30,
-            y: WINDOW_HEIGHT / 2 + 30,
-            velocityX: 1,
+            //x: 0,
+            y: 0,
+            velocityX: 0,
             velocityY: 0,
-            charge: ELECTRON_CHARGE * 20    ,
+            charge: ELECTRON_CHARGE * 75,
             mass: ELECTRON_MASS,
         });
-
+/*
         var p4 = new Particle({
-            //x: WINDOW_WIDTH / 2 - 30,
-            y: WINDOW_HEIGHT / 2 - 30,
-            velocityX: -1,
+            //x: 0,
+            y: 0,
+            velocityX: 0,
             velocityY: 0,
-            charge: ELECTRON_CHARGE * 20    ,
+            charge: ELECTRON_CHARGE * 75,
             mass: ELECTRON_MASS,
         });
-
+*/
         this.particles = [
-            p0, p1,  p4
+            p0, p1//, p2//, p3, p4
         ];
     }
 
@@ -168,7 +185,7 @@ class TwoPointChargeSystem {
             for(var i = 0; i < this.particles.length; ++i){
                 this.particles[i].advanceTime(this.frameMillis);
             }
-        }, this), this.frameMillis);
+        }, this), 1000/60/*this.frameMillis*/);
         this.disableInputs();
         this.running = true;
         this.renameStartResetButton();
